f08139023b8cfd680f7d7d1896266623d4dacc70
junpei yamaguchi <yamaguchi.cocytos@gmail.com>
2016-07-14 13:48:25 +0900

    title add

--- src/resource.js
+++ src/resource.js
@@ -1,10 +1,7 @@
 var res = {
     background_png : "res/background.png",
-    ship_png : "res/ship.png",
-    particle_png : "res/particle.png",
-    asteroid_png : "res/asteroid.png",
-    bgm_main : "res/bgm_main.mp3",
-    se_bang : "res/se_bang.mp3"
+    Title_png:"res/Title.png",
+    start_png:"res/start.png"
 };
 
 var g_resources = [];
